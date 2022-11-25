72452dc7f09d48b41194c2c4fe35be0828113506
PabloKinsel <106718950+PabloKinsel@users.noreply.github.com>
2022-11-25 01:03:27 -0300

    com atraso

--- trabalho_final/src/App.js
+++ trabalho_final/src/App.js
@@ -1,15 +1,23 @@
+import React from 'react';
 import './App.css';
 
 
 const Vitor = new URL("./vitor.png", import.meta.url);
 const Pablo = new URL("./pablo.jpg", import.meta.url);
-//const Alexandre = new URL("./.jpg", import.meta.url);
+const Alexandre = new URL("./Alexandre.jpg", import.meta.url);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-     
+
+      <div class="escolha"> 
+        <p>3. Criar uma página para controlar uma lista de compras. 
+            Consiste em um campo para adicionar itens na lista.
+            com a possibilidade de editar, excluir 
+            marcar que já pegou o item. </p>
+      </div>
+
       <div class="card">
             <div class="imgBox">
             <img src={Pablo}/>
@@ -30,13 +38,14 @@ function App() {
 
       <div class="card">
             <div class="imgBox">
-            <img src={Vitor}/> // colocar imgaem Alexandre;
+            <img src={Alexandre}/>
             </div>          
             <div class="content">
                     <h2>Alexandre</h2>
             </div>
             </div>
 
+     
       </header>
     </div>
   );
